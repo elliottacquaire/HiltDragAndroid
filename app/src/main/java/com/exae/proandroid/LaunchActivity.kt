b0b9b7b77587aabf84794c7d09d2a838ecf56b99
xuyang.li <xuyangl@wicresoft.com>
2021-04-07 18:02:01 +0800

    协程的使用 kotlin

--- app/src/main/java/com/exae/proandroid/LaunchActivity.kt
+++ app/src/main/java/com/exae/proandroid/LaunchActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import com.exae.proandroid.base.PosBaseActivity
 import com.exae.proandroid.bean.TestBean
 import com.exae.proandroid.bean.TestData
+import com.exae.proandroid.coroutines.CoroutinesUtil
 import com.exae.proandroid.utils.CommonUtils
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_launch.*
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -26,7 +28,60 @@ class LaunchActivity : PosBaseActivity(R.layout.activity_launch) {
         butions.setOnClickListener {
 //            startActivity(Intent(this,MainActivity::class.java))
 //            CommonUtils.tt()
-            CommonUtils.apply_Let_With_Run()
+//            CommonUtils.apply_Let_With_Run()
+
+            /**
+             * 默认的协程调度器就是Dispatchers.Default，Default是一个协程调度器，其指定的线程为共有的线程池，线程数量至少为 2 最大与 CPU 数相同。
+             */
+            GlobalScope.launch(Dispatchers.Main) { // 在 UI 线程创建一个新协程 // Dispatchers.Main可以省略
+                delay(3000)
+                val token = CoroutinesUtil.createToken("tokense")
+                Logger.d("=444=======$token=======")
+
+                //这块指定网络请求使用IO线程，withContext {}不会创建新的协程，在指定协程上运行挂起代码块，并挂起该协程直至代码块运行完成。
+                //withContext很适合需要直接获取协程结果但又不会短时间内重复调用的场景,比如网络请求.
+                //async适用于需要多次调用并且需要知道结果的场景,比如多任务下载.
+                // 如果我不需要多次调用只需要知道结果,还可以使用withContext,withContext的效果类似于上面代码的async{…}.await(),
+                // 不同的是withContext没有默认调度器必须要指定一个协程调度器,
+
+                val token1 = withContext(Dispatchers.IO){
+                    CoroutinesUtil.createToken("tokense")
+                }
+                Logger.d("=555=======$token1=======")
+
+
+            }
+            Logger.d("=333111=======${Thread.currentThread().name}=======")
+
+
+            //是创建一个新的协程同时阻塞当前线程，直到协程结束。这个不应该在协程中使用，主要是为main函数和测试设计的。
+            //但是launch不是suspend函数,runblocking不会等待非suspend函数),我该结束了,于是,它结束了程序,
+            // 而launch闭包中的延时还没来得及执行,程序就被关闭了,所以two没有被打印出来
+            runBlocking {
+                Logger.d("------runBlocking  start------------")
+                delay(5000)
+                Logger.d("------runBlocking  end------------")
+            }
+            Logger.d("=333=======${Thread.currentThread().name}=======")
+
+
+            val defered = GlobalScope.async {
+                Logger.d("协程1开始执行")
+                delay(3000)
+                val token = CoroutinesUtil.createToken("tokense")
+                Logger.d("=async=======$token=======")
+            }
+            if (defered.isCompleted){
+                val res = defered.getCompleted()
+                Logger.d("=async=======$res=======")
+            }else{
+                Logger.d("=async=======no result=======")
+            }
+
+
+
+
+
         }
 
     }
---
+++ app/src/main/java/com/exae/proandroid/coroutines/CoroutinesUtil.kt
@@ -0,0 +1,64 @@
+package com.exae.proandroid.coroutines
+
+import com.orhanobut.logger.Logger
+import kotlinx.coroutines.delay
+
+/**
+ * 协程就像非常轻量级的线程。线程是由系统调度的，线程切换或线程阻塞的开销都比较大。而协程依赖于线程，但是协程挂起时不需要阻塞线程，
+ * 几乎是无代价的，协程是由开发者控制的。所以协程也像用户态的线程，非常轻量级，一个线程中可以创建任意个协程。
+
+ * 协程可以简化异步编程，可以顺序地表达程序，协程也提供了一种避免阻塞线程并用更廉价、更可控的操作替代线程阻塞的方法 -- 协程挂起。
+ *
+ * 协程scope
+    协程代码都必须包含在一个范围内，就好比说：只有在这个圈里可以使用协程。这个scope用于跟踪管理其内部的协程。
+
+    例如：协程标准库中的GlobalScope 、Android的viewModelScope
+
+    协程builder
+    其协程代码与非协程代码的桥梁。其负责在非协程代码里启动协程，即在协程scope里build一个协程代码块
+
+    例如：launch与async
+
+    协程dispatcher
+    它负责具体执行线程。使用协程builder构建了协程后，总的有人去执行啊，这个任务就是由dispatcher完成的，它会调度线程来运行协程代码。
+
+    例如：Dispatchers.Default、Dispatchers.IO，Dispatchers.Main
+
+    CoroutineStart
+    通过 launch{} 等创建一个协程的时候，你也可以传入一个 CoroutineStart 枚举值，这个枚举值参数定义了 CoroutineBuilder 的执行 Coroutine 的时机，具体的时机由以下几种：
+
+    DEFAULTE:会根据该 Coroutine 依赖的 Context 立刻执行该 Coroutine
+    LAZY:按需执行 Coroutine，仅仅在你调用了 Job.start() 或者 Job.await() 之后会执行
+    ATOMIC: 原子操作类型，也就是说会根据依赖的Context 执行 Coroutine，但是该 Coroutine 不可取消。对比 DEFAULT 类型，它不不可取消，不能通过 job.cancel() 去取消的。
+    UNDISPATCHED:会在当前线程第一个挂起点执行 Coroutine
+
+ */
+object CoroutinesUtil {
+
+    /**
+     * 函数前面有suspend修饰符标记，这表示两个函数都是挂起函数。
+     * 挂起函数能够以与普通函数相同的方式获取参数和返回值，但是调用函数可能挂起协程（如果相关调用的结果已经可用，
+     * 库可以决定继续进行而不挂起），挂起函数挂起协程时，不会阻塞协程所在的线程。
+     * 挂起函数执行完成后会恢复协程，后面的代码才会继续执行。但是挂起函数只能在协程中或其他挂起函数中调用。
+     * 事实上，要启动协程，至少要有一个挂起函数，它通常是一个挂起 lambda 表达式。所以suspend修饰符可以标记普通函数、扩展函数和 lambda 表达式
+     */
+    suspend fun createToken(tt : String) : String{
+        delay(2000)
+        Logger.d("*111***$tt*********${Thread.currentThread().name}********")
+        val  name = getName(30)
+        return "*****$tt-----$name--"
+    }
+
+    suspend fun getName(age:Int) : String{
+        Logger.d("**222*****$age******${Thread.currentThread().name}********")
+        return if (age > 20){
+            "jakc_20"
+        }else {
+            "jakc_10"
+        }
+    }
+
+    fun testSS() : Int{
+        return 30
+    }
+}
